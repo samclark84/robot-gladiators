0ec5ddca66a1cca950a5477751d711fff05f4739
Sam Clark <samuel.clark84@yahoo.com>
2022-03-29 05:21:31 -0500

    initial javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is you robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}    
+
+// fight();
